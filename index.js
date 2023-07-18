eac85b60343b03ebde5682890fea811beaf7ca80
DannieB23 <137961298+DannieB23@users.noreply.github.com>
2023-07-18 11:09:58 -0700

    Added more cool stuff

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 let itemText = document.getElementById("add-input");
 let button = document.querySelector(".button-submit");
 let outputItem = document.querySelector("#listBox");
+let buttonDelete = document.querySelector(".button-delete");
 //alert("Alert");
 
 button.addEventListener("click", itemCreation);
+
 
 function itemCreation() {
     let newItemElement = document.createElement('p');
     let newItem = document.createTextNode(itemText.value);
     newItemElement.appendChild(newItem);
-    outputItem.appendChild(newItemElement);    
+    outputItem.appendChild(newItemElement);
 
     //outputItem.appendChild(itemText);
     //outputItem.innerHTML = itemText.value;
-    alert("Child Appended");
+    // alert("Child Appended");
+
+    buttonDelete.addEventListener('click', function () {
+        newItemElement.remove();
+        itemElements.pop();
+    });
+
+    newItemElement.appendChild(removeButton);
+    outputItem.appendChild(newItemElement);
+    itemElements.push(newItemElement);
+
 }
